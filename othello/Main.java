282198b060cce433af3ab733214573d8108b4992
vc7 <vince78718@gmail.com>
2020-03-06 13:04:23 +0900

    Able to step initial state, with 2 blacks 2 whites, of the board. Add some basic checking methods.

--- othello/Main.java
+++ othello/Main.java
@@ -1,9 +1,28 @@
 package othello;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         Othello othello = new Othello();
         othello.reset();
+        
+        while (!othello.isFinished()) {
+            System.out.println(othello.toString());
+            System.out.println("Next move, type \"EXIT\" to exit: ");
+
+            // TODO: 取得下一步可以走的顏色
+
+            String command = scanner.next();
+            System.out.println(command);
+            if (command.equals("EXIT")) {
+                break;
+            }
+        }
+
         System.out.println(othello.toString());
+        System.out.println(othello.result());
     }
 }
--- othello/Othello.java
+++ othello/Othello.java
@@ -5,6 +5,8 @@ import java.util.*;
 class Othello {
 
     private String[][] board;
+    private Coordinate[] whites;
+    private Coordinate[] blacks;
 
     /**
      * 覆寫 toString 方法，直接印出棋盤的內容
@@ -23,6 +25,9 @@ class Othello {
             }
             string += "\n";
         }
+        string += "\n";
+        string += "b: " + blacks.length + "\n";
+        string += "w: " + whites.length + "\n";
         return string;
     }
 
@@ -31,6 +36,37 @@ class Othello {
      */
     void reset() {
         board = createEmptyBoard();
+        insertInitialStones();
+    }
+
+    String result() {
+        if (blacks.length == whites.length) {
+            return "Result: Draw";
+        }
+
+        return "Result " + ((blacks.length > whites.length) ? "Black" : "White")+  "win";
+    }
+
+    Move nextMove() {
+        // TODO: 回傳下一步可以走的位置
+        return null;
+    }
+
+    /**
+     * 遊戲是否結束
+     * <p> 結束條件：滿盤 或是 黑和白都沒有地方走
+     */
+    public boolean isFinished() {
+        // TODO: 實作條件判斷
+        return false;
+    }
+
+    /**
+     * 目前盤面是否已滿
+     */
+    private boolean isFull() {
+        // TODO: 錯誤判斷
+        return blacks.length + whites.length == 64;
     }
 
     /**
@@ -38,9 +74,81 @@ class Othello {
      */
     private String[][] createEmptyBoard() {
         String[][] emptyBoard = new String[8][8];
-        String[] row = new String[8];
-        Arrays.fill(row, ".");
-        Arrays.fill(emptyBoard, row);
+        for (int row = 0; row < 8; row++) {
+            for (int column = 0; column < 8; column++) {
+                emptyBoard[row][column] = ".";
+            }
+        }
         return emptyBoard;
+    }
+
+    /**
+     * 放入初始的四個棋子。
+     * <p>黑: d5 (3, 4), e4 (4, 3)
+     * <p>白: d4 (3, 3), e5 (4, 4)
+     * 
+     * @param board 目標棋盤
+     */
+    private void insertInitialStones() {
+        blacks = new Coordinate[] {
+            new Coordinate(3, 4),
+            new Coordinate(4, 3)
+        };
+        whites = new Coordinate[] {
+            new Coordinate(3, 3),
+            new Coordinate(4, 4)
+        };
+        updateBoard();
+    }
+
+    /**
+     * 根據現在的旗子更新盤面
+     * 註記：可能不需要走訪全部的 blacks 和 whites
+     */
+    private void updateBoard() {
+        for (int index = 0; index < blacks.length; index++) {
+            Coordinate coordinate = blacks[index];
+            board[coordinate.y][coordinate.x] = "b";
+        }
+
+        for (int index = 0; index < whites.length; index++) {
+            Coordinate coordinate = whites[index];
+            board[coordinate.y][coordinate.x] = "w";
+        }
+    }
+
+    // ------------------------------------------
+
+    /**
+     * 座標用 Class
+     */
+    private class Coordinate {
+        int x;
+        int y;
+
+        Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + x + ", " + y + ")";
+        }
+    }
+
+    // ------------------------------------------
+
+    /**
+     * 棋步
+     */
+    class Move {
+        boolean isBlack;
+        Coordinate[] possibleMoves;
+
+        Move() {
+            isBlack = true;
+            possibleMoves = new Coordinate[] {};
+        }
     }
 }
